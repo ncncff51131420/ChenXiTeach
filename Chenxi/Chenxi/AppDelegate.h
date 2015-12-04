d04d842cf76a6f59da840c1ac768a9ee71e75c10
MS <1170855052@qq.com>
2015-12-04 17:44:27 +0800

    这是第一次提交

---
+++ Chenxi/Chenxi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chenxi
+//
+//  Created by MS on 15-12-4.
+//  Copyright (c) 2015年 李海星. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
